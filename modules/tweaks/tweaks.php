1a2f1ff4a8fec45ba046b36a7d7fad0d577d5ad9
Nasser Rafie <contact@geminorum.ir>
2016-04-19 16:16:39 +0430

    github wiki help tab

--- modules/tweaks/tweaks.php
+++ modules/tweaks/tweaks.php
@@ -23,24 +23,25 @@ class gEditorialTweaks extends gEditorialModuleCore
 
 	protected function settings_help_tabs()
 	{
-		return array(
-			array(
-				'id'       => 'geditorial-tweaks-checklist_tree',
-				'title'    => _x( 'Checklist Tree', 'Tweaks Module: Help Tab Title', GEDITORIAL_TEXTDOMAIN ),
-				'content'  => '<div class="-info"><p>If you’ve ever used categories extensively, you will have noticed that after you save a post, the checked categories appear on top of all the other ones. This can be useful if you have a lot of categories, since you don’t have to scroll.</p>
+		$tabs = gEditorialHelper::settingsHelpContent( $this->module );
+
+		$tabs[] = array(
+			'id'       => 'geditorial-tweaks-checklist_tree',
+			'title'    => _x( 'Checklist Tree', 'Tweaks Module: Help Tab Title', GEDITORIAL_TEXTDOMAIN ),
+			'content'  => '<div class="-info"><p>If you’ve ever used categories extensively, you will have noticed that after you save a post, the checked categories appear on top of all the other ones. This can be useful if you have a lot of categories, since you don’t have to scroll.</p>
 <p>Unfortunately, this behaviour has a serious side-effect: it breaks the hierarchy. If you have deeply nested categories that don’t make sense out of context, this will completely screw you over.</p>
 <p>It preserves the category tree at all times. Just activate it and you’re good.</p>
 <p class="-from">Adopted from: <a href="https://wordpress.org/plugins/category-checklist-tree/" target="_blank">Category Checklist Tree</a> by <a href="http://scribu.net/wordpress/category-checklist-tree" target="_blank">scribu</a></p></div>',
-				'callback' => FALSE,
-			),
-			array(
-				'id'       => 'geditorial-tweaks-category_search',
-				'title'    => _x( 'Category Search', 'Tweaks Module: Help Tab Title', GEDITORIAL_TEXTDOMAIN ),
-				'content'  => '<div class="-info"><p>Makes it quick and easy for writers to select categories related to what they are writing. As they type in the search box, categories will be shown and hidden in real time, allowing them to easily select what is relevant to their content without having to scroll through possibly hundreds of categories.</p>
-<p class="-from">Adopted from: <a href="https://wordpress.org/plugins/searchable-categories/" target="_blank">Searchable Categories</a> by <a href="http://ididntbreak.it" target="_blank">Jason Corradino</a></p></div>',
-				'callback' => FALSE,
-			),
 		);
+
+		$tabs[] = array(
+			'id'       => 'geditorial-tweaks-category_search',
+			'title'    => _x( 'Category Search', 'Tweaks Module: Help Tab Title', GEDITORIAL_TEXTDOMAIN ),
+			'content'  => '<div class="-info"><p>Makes it quick and easy for writers to select categories related to what they are writing. As they type in the search box, categories will be shown and hidden in real time, allowing them to easily select what is relevant to their content without having to scroll through possibly hundreds of categories.</p>
+<p class="-from">Adopted from: <a href="https://wordpress.org/plugins/searchable-categories/" target="_blank">Searchable Categories</a> by <a href="http://ididntbreak.it" target="_blank">Jason Corradino</a></p></div>',
+		);
+
+		return $tabs;
 	}
 
 	protected function get_global_settings()
