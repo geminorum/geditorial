5111887c34e0d7894e5b5cd974f09407e85b9ab2
Nasser Rafie <contact@geminorum.ir>
2016-08-28 07:44:08 +0430

    :new: module: home

---
+++ modules/home/home.php
@@ -0,0 +1,37 @@
+<?php defined( 'ABSPATH' ) or die( 'Restricted access' );
+
+class gEditorialHome extends gEditorialModuleCore
+{
+
+	public static function module()
+	{
+		return array(
+			'name'     => 'home',
+			'title'    => _x( 'Home', 'Home Module', GEDITORIAL_TEXTDOMAIN ),
+			'desc'     => _x( 'Home Page Customized', 'Home Module', GEDITORIAL_TEXTDOMAIN ),
+			'dashicon' => 'admin-home',
+		);
+	}
+
+	protected function get_global_settings()
+	{
+		return array(
+			'posttypes_option' => 'posttypes_option',
+		);
+	}
+
+	public function init()
+	{
+		do_action( 'geditorial_home_init', $this->module );
+		$this->do_globals();
+
+		if ( ! is_admin() && count( $this->post_types() ) )
+			add_action( 'pre_get_posts', array( $this, 'pre_get_posts' ), 9 );
+	}
+
+	public function pre_get_posts( &$query )
+	{
+		if ( is_home() && $query->is_main_query() )
+			$query->set( 'post_type', $this->post_types() );
+	}
+}
