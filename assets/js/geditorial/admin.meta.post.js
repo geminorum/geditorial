e575525217abf686f0c9792aa950583eba31c172
Nasser Rafie <contact@geminorum.ir>
2017-01-22 13:59:29 +0330

    module/meta: reorder tab index if subtitle field exists

--- assets/js/geditorial/admin.meta.post.js
+++ assets/js/geditorial/admin.meta.post.js
@@ -12,4 +12,33 @@ jQuery(document).ready(function($) {
   $("[data-meta-type='box']").each(function() {
     $(this).parents('div.postbox').appendTo("#titlediv");
   });
+
+  // wait till post.js binds
+  $(window).bind('load', function() {
+
+    if ( $("[data-meta-type='title_after']").length ) {
+      $('#title').unbind('keydown.editor-focus');
+
+      // copy from post.js
+      // This code is meant to allow tabbing from Title to Post content.
+      $("[data-meta-type='title_after']").on( 'keydown.editor-focus', function( event ) {
+        var editor,
+          $textarea = $('#content');
+
+        if ( event.keyCode === 9 && ! event.ctrlKey && ! event.altKey && ! event.shiftKey ) {
+          editor = typeof tinymce != 'undefined' && tinymce.get('content');
+
+          if ( editor && ! editor.isHidden() ) {
+            editor.focus();
+          } else if ( $textarea.length ) {
+            $textarea.focus();
+          } else {
+            return;
+          }
+
+          event.preventDefault();
+        }
+      });
+    };
+  });
 });
