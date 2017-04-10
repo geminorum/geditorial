86fa0954ce43a61bc8b488886d950a8a811c8646
Nasser Rafie <contact@geminorum.ir>
2017-04-10 14:31:09 +0430

    :wrench: bump min php ver to 5.4

--- geditorial.php
+++ geditorial.php
@@ -15,7 +15,7 @@ RepoGitHub: geminorum/geditorial
 GitHub Plugin URI: https://github.com/geminorum/geditorial
 GitHub Branch: master
 Requires WP: 4.4
-Requires PHP: 5.3
+Requires PHP: 5.4
 */
 
 define( 'GEDITORIAL_VERSION', '3.9.15' );
