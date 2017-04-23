290d48980e6c0998e6a3a8b0948a19598df2c7ac
Nasser Rafie <contact@geminorum.ir>
2017-04-23 16:56:44 +0430

    module/settings: missed js mods!

--- assets/js/geditorial/admin.settings.js
+++ assets/js/geditorial/admin.settings.js
@@ -1,5 +1,25 @@
 jQuery(document).ready(function($) {
 
+  var moduleList = new List('geditorial-settings', {
+      listClass: '-list',
+      searchClass: '-search',
+      valueNames: [ '-title', '-description', '-th' ]
+  });
+
+  // https://github.com/javve/list.js/issues/366#issuecomment-274942284
+  moduleList.on('updated', function (list) {
+
+    // also .clear container
+    if ( ( list.items.length - 1 ) == list.matchingItems.length )
+      return;
+
+    // every updated item take animation with their id
+    list.matchingItems.forEach(function (element) {
+        var id = element.elm.id;
+        $('#' + id).addClass('animated fadeIn');
+    });
+  });
+
   $('.fields-check-all').click(function(e) {
     $(this).closest('table').find('.fields-check').prop('checked', this.checked);
   });
