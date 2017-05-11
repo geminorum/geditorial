1903a2580c63317f73f6616769160bdf161d4bcc
Nasser Rafie <contact@geminorum.ir>
2017-05-11 15:51:24 +0430

    module/drafts: :new: public preview

---
+++ assets/js/admin.drafts.post.js
@@ -0,0 +1,61 @@
+(function($, p, c, m) {
+  'use strict';
+
+  var o = {};
+
+  o.action = p._base + '_' + m;
+
+  o.a = function(el) {
+
+    var $el = $(el),
+      $row = $el.parents('.geditorial-admin-wrap'),
+      $input = $row.find('input.-link'),
+      action = $el.data('action'),
+      $spinner = $row.find('.spinner');
+
+    $.ajax({
+      url: p._url,
+      method: 'POST',
+      data: {
+        action: o.action,
+        what: action,
+        post_id: $el.data('id'),
+        nonce: $el.data('nonce')
+      },
+      beforeSend: function(xhr) {
+        $spinner.addClass('is-active');
+      },
+      success: function(response, textStatus, xhr) {
+        $spinner.removeClass('is-active');
+
+        if (response.success) {
+
+          if ( action === 'public' ) {
+            $input.val( response.data.link ).show();
+          } else {
+            $input.hide();
+          }
+
+          $el.hide();
+          $row.find('a.-after-'+action).show();
+
+        } else {
+          console.log(response);
+        }
+      }
+    });
+  };
+
+  $(document).ready(function() {
+    $('.geditorial-admin-wrap.-drafts a.-action').on('click', function(e) {
+      e.preventDefault();
+      o.a(this);
+    });
+  });
+
+  c[m] = o;
+
+  if (p._dev)
+    console.log(o);
+
+}(jQuery, gEditorial, gEditorialModules, 'drafts'));
--- modules/drafts/drafts.php
+++ modules/drafts/drafts.php
@@ -38,7 +38,19 @@ class Drafts extends gEditorial\Module
 					'description' => _x( 'Display maximum posts for each post type', 'Modules: Drafts: Setting Description', GEDITORIAL_TEXTDOMAIN ),
 					'default'     => 100,
 				],
+				[
+					'field'       => 'public_preview',
+					'title'       => _x( 'Public Preview', 'Modules: Drafts: Setting Title', GEDITORIAL_TEXTDOMAIN ),
+					'description' => _x( 'Provide a secret link to non-logged in users to view post drafts', 'Modules: Drafts: Setting Description', GEDITORIAL_TEXTDOMAIN ),
+				],
 			],
+		];
+	}
+
+	protected function get_global_constants()
+	{
+		return [
+			'meta_secret' => '_preview_secret',
 		];
 	}
 
@@ -46,11 +58,7 @@ class Drafts extends gEditorial\Module
 	{
 		parent::init();
 
-
-		if ( is_admin() ) {
-
-
-		} else {
+		if ( ! is_admin() ) {
 
 			if ( is_admin_bar_showing()
 				&& $this->cuc( 'ajax' )
@@ -61,12 +69,31 @@ class Drafts extends gEditorial\Module
 				$this->enqueue_styles();
 			}
 
+			if ( $this->get_setting( 'public_preview', FALSE ) )
+				$this->filter( 'the_posts', 2 );
 		}
 	}
 
 	public function init_ajax()
 	{
 		$this->_hook_ajax();
+	}
+
+	public function current_screen( $screen )
+	{
+		if ( ! $this->get_setting( 'public_preview', FALSE ) )
+			return;
+
+		if ( in_array( $screen->post_type, $this->post_types() ) ) {
+			if ( 'post' == $screen->base ) {
+				$this->action( 'post_submitbox_minor_actions', 1, 12 );
+				$this->enqueue_asset_js( [], $screen );
+
+			} else if ( 'edit' == $screen->base ) {
+				$this->filter( 'post_row_actions', 2 );
+				add_action( 'geditorial_tweaks_column_attr', [ $this, 'column_attr' ], 90 );
+			}
+		}
 	}
 
 	public function admin_bar_menu( $wp_admin_bar )
@@ -81,17 +108,53 @@ class Drafts extends gEditorial\Module
 
 	public function ajax()
 	{
-		Ajax::checkReferer( $this->hook() );
-
-		if ( ! $this->cuc( 'ajax' ) )
-			self::cheatin();
-
 		$post = wp_unslash( $_POST );
 		$what = empty( $post['what'] ) ? 'nothing': trim( $post['what'] );
 
 		switch ( $what ) {
 
-			case 'list': Ajax::successHTML( $this->drafts_list() );
+			case 'private':
+
+				if ( empty( $post['post_id'] ) )
+					Ajax::errorMessage();
+
+				if ( ! current_user_can( 'edit_post', $post['post_id'] ) )
+					Ajax::errorMessage();
+
+				if ( ! $this->nonce_verify( $post['nonce'], $post['post_id'] ) )
+					self::cheatin();
+
+				if ( ! $this->make_private( $post['post_id'] ) )
+					Ajax::errorMessage( _x( 'Unable to make preview private. Please try again.', 'Modules: Drafts', GEDITORIAL_TEXTDOMAIN ) );
+
+				Ajax::successMessage();
+
+			break;
+			case 'public':
+
+				if ( empty( $post['post_id'] ) )
+					Ajax::errorMessage();
+
+				if ( ! current_user_can( 'edit_post', $post['post_id'] ) )
+					Ajax::errorMessage();
+
+				if ( ! $this->nonce_verify( $post['nonce'], $post['post_id'] ) )
+					self::cheatin();
+
+				if ( ! $link = $this->make_public( $post['post_id'] ) )
+					Ajax::errorMessage( _x( 'Unable to make preview public. Please try again.', 'Modules: Drafts', GEDITORIAL_TEXTDOMAIN ) );
+
+				wp_send_json_success( [ 'link' => $link ] );
+
+			break;
+			case 'list':
+
+				Ajax::checkReferer( $this->hook() );
+
+				if ( ! $this->cuc( 'ajax' ) )
+					self::cheatin();
+
+				Ajax::successHTML( $this->drafts_list() );
 		}
 
 		Ajax::errorWhat();
@@ -149,5 +212,123 @@ class Drafts extends gEditorial\Module
 
 		$query = new \WP_Query( $args );
 		return $query->posts;
+	}
+
+	public function post_submitbox_minor_actions( $post )
+	{
+		$allowed = $this->filters( 'preview_statuses', [ 'draft', 'pending', 'auto-draft', 'future' ] );
+
+		if ( ! in_array( get_post_status( $post ), $allowed ) )
+			return;
+
+		$public = $this->is_public( $post->ID );
+		$nonce  = $this->nonce_create( $post->ID );
+
+		echo '<div class="geditorial-admin-wrap -drafts">';
+
+		echo HTML::tag( 'input', [
+			'type'     => 'text',
+			'value'    => $this->get_preview_url( $post->ID ),
+			'style'    => $public ? FALSE : 'display:none;',
+			'class'    => [ 'widefat', '-link' ],
+			'readonly' => TRUE,
+			'onclick'  => 'this.focus();this.select()',
+		] );
+
+		echo '<span class="-loading spinner"></span>';
+
+		echo HTML::tag( 'a', [
+			'href'  => '#',
+			'class' => [ 'button', 'button-small', '-button', '-action', '-after-private' ],
+			'style' => $public ? 'display:none;' : FALSE,
+			'data'  => [
+				'id'     => $post->ID,
+				'action' => 'public',
+				'nonce'  => $nonce,
+			],
+		], _x( 'Make Preview Public', 'Modules: Drafts', GEDITORIAL_TEXTDOMAIN ) );
+
+		echo HTML::tag( 'a', [
+			'href'  => '#',
+			'class' => [ 'button', 'button-small', '-button', '-action', '-after-public' ],
+			'style' => $public ? FALSE : 'display:none;',
+			'data'  => [
+				'id'     => $post->ID,
+				'action' => 'private',
+				'nonce'  => $nonce,
+			],
+		], _x( 'Make Preview Private', 'Modules: Drafts', GEDITORIAL_TEXTDOMAIN ) );
+
+		echo '</div>';
+	}
+
+	public function get_preview_url( $post_id, $key = NULL )
+	{
+		$url = get_permalink( $post_id );
+
+		if ( is_null( $key ) )
+			$key = get_post_meta( $post_id, $this->constant( 'meta_secret' ), TRUE );
+
+		return $key ? add_query_arg( 'secret', $key, $url ) : $url;
+	}
+
+	public function make_private( $post_id )
+	{
+		return delete_post_meta( $post_id, $this->constant( 'meta_secret' ) );
+	}
+
+	public function make_public( $post_id )
+	{
+		$key   = wp_generate_password( 6, FALSE, FALSE );
+		$added = add_post_meta( $post_id, $this->constant( 'meta_secret' ), $key, TRUE );
+
+		return $added ? $this->get_preview_url( $post_id, $key ) : FALSE;
+	}
+
+	public function is_public( $post_id )
+	{
+		return strlen( get_post_meta( $post_id, $this->constant( 'meta_secret' ), TRUE ) ) > 0;
+	}
+
+	public function the_posts( $posts, $wp_query )
+	{
+		global $wpdb;
+
+		if ( isset( $_GET['secret'] )
+			&& $wp_query->is_main_query()
+			&& get_post_meta( $wp_query->query_vars['p'], $this->constant( 'meta_secret' ), TRUE ) === $_GET['secret'] )
+				$posts = $wpdb->get_results( $wp_query->request );
+
+		return $posts;
+	}
+
+	public function post_row_actions( $actions, $post )
+	{
+		if ( $this->is_public( $post->ID ) )
+			$actions['public_link'] = HTML::link(
+				_x( 'Public Preview', 'Modules: Drafts', GEDITORIAL_TEXTDOMAIN ),
+				$this->get_preview_url( $post->ID )
+			);
+
+		return $actions;
+	}
+
+	public function column_attr( $post )
+	{
+		if ( ! $this->is_public( $post->ID ) )
+			return;
+
+		$link = $this->get_preview_url( $post->ID );
+
+		echo '<li class="-attr -drafts -preview-link">';
+			echo $this->get_column_icon( FALSE, 'welcome-view-site', _x( 'Preview', 'Modules: Drafts: Row Icon Title', GEDITORIAL_TEXTDOMAIN ) );
+
+			echo HTML::tag( 'a', [
+				'href'   => $link,
+				'class'  => '-link',
+				'target' => '_blank',
+			], _x( 'Has public preview link', 'Modules: Drafts', GEDITORIAL_TEXTDOMAIN ) );
+
+		echo '</li>';
 	}
 }
