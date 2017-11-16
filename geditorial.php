63f51544bf722a21b7d0e96f2ff7f86d6fe27970
Nasser Rafie <contact@geminorum.ir>
2017-11-16 16:44:34 +0330

    main/main: check for minimum required veriosn of php

--- geditorial.php
+++ geditorial.php
@@ -20,13 +20,22 @@ Requires PHP: 5.4
 */
 
 define( 'GEDITORIAL_VERSION', '3.13.1' );
+define( 'GEDITORIAL_MIN_PHP', '5.4.0' );
 define( 'GEDITORIAL_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GEDITORIAL_URL', plugin_dir_url( __FILE__ ) );
 define( 'GEDITORIAL_FILE', basename( GEDITORIAL_DIR ).'/'.basename( __FILE__ ) );
 
 defined( 'GEDITORIAL_TEXTDOMAIN' ) or define( 'GEDITORIAL_TEXTDOMAIN', 'geditorial' );
 
-if ( file_exists( GEDITORIAL_DIR.'assets/vendor/autoload.php' ) ) {
+if ( version_compare( GEDITORIAL_MIN_PHP, PHP_VERSION, '>=' ) ) {
+
+	echo '<div class="notice notice-warning notice-alt is-dismissible"><p dir="ltr">';
+		printf( '<b>gEditorial</b> requires PHP %s or higher. Please contact your hosting provider to update your site.', GEDITORIAL_MIN_PHP ) ;
+	echo '</p></div>';
+
+	return FALSE;
+
+} else if ( file_exists( GEDITORIAL_DIR.'assets/vendor/autoload.php' ) ) {
 	require_once( GEDITORIAL_DIR.'assets/vendor/autoload.php' );
 
 	require_once( GEDITORIAL_DIR.'includes/plugin.php' );
