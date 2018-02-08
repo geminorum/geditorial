cde26bf3e326e5fca1beb8d96e481646b0989af8
Nasser Rafie <contact@geminorum.ir>
2018-02-09 00:38:40 +0330

    misc/walker/user: user avatar

--- includes/misc/walker-user-checklist.php
+++ includes/misc/walker-user-checklist.php
@@ -38,9 +38,10 @@ class Walker_User_Checklist extends \Walker
 				// $icon_after   = HTML::getDashicon( 'yes' ); // working but no need
 			}
 
-			$output.= '<li'.$class.'>'
+			$output.= '<li class="-user">'
 				.'<div class="'.$inner_class.'" data-term-id='.$term->term_id
 				.' tabindex="0" role="checkbox" aria-checked="'.$aria_checked.'">'
+				.( get_option( 'show_avatars' ) ? get_avatar( $user->ID, 16 ) : '' )
 				.'<span class="-name">'.HTML::escape( $user->display_name ).'</span> '
 				.'<code class="-login">&#8206;@'.$user->user_login.'&#8207;</code>'
 				.$icon_after.'</div>';
@@ -55,7 +56,7 @@ class Walker_User_Checklist extends \Walker
 					'checked'  => in_array( $user->user_login, (array) $args['selected'] ),
 					'disabled' => ! empty( $args['disabled'] ) || ! $value,
 				] )
-				// FIXME: add floating avatar
+				.( get_option( 'show_avatars' ) ? get_avatar( $user->ID, 32 ) : '' )
 				.' <span class="-name">'.HTML::escape( $user->display_name ).'</span>'
 				.' <code class="-login">&#8206;@'.$user->user_login.'&#8207;</code>'
 				.'<br /><span class="-email code">'.HTML::mailto( $user->user_email ).'</span></label>';
