22d269032063be37915bc4869d041355002896f1
Nasser Rafie <contact@geminorum.ir>
2018-02-25 10:22:57 +0330

    main/ajax: js module container deprecated

--- includes/ajax.php
+++ includes/ajax.php
@@ -89,9 +89,11 @@ class Ajax extends Core\Base
 
 	?><script type="text/javascript">
 /* <![CDATA[ */
-	var <?php echo $object.'Modules'; ?> = {};
 	var <?php echo $object; ?> = <?php echo wp_json_encode( $props ); ?>;
-	<?php if ( $props['_dev'] ) echo 'console.log('.$object.');'."\n"; ?>
+	<?php if ( $props['_dev'] ) {
+		echo 'console.log("'.$object.'", '.$object.');'."\n";
+		echo "\t".'jQuery(document).on("gEditorialReady", function(e, module, app){console.log(module, app);});'."\n";
+	} ?>
 /* ]]> */
 </script><?php
 	}
