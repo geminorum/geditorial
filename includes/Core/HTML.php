ea78c81f13c2042fc0114d5c7e1cb459be1555ff
Nasser Rafie <contact@geminorum.ir>
2019-03-19 09:12:34 +0330

    :peach:

--- includes/Core/HTML.php
+++ includes/Core/HTML.php
@@ -17,7 +17,7 @@ class HTML extends Base
 
 	public static function mailto( $email, $title = NULL, $wrap = FALSE )
 	{
-		$title = $title ? $title : trim( $email );
+		$title = $title ? $title : self::wrapLTR( trim( $email ) );
 		$link  = '<a class="-mailto" href="mailto:'.trim( $email ).'">'.$title.'</a>';
 		return $wrap ? self::tag( $wrap, $link ) : $link;
 	}
@@ -302,6 +302,11 @@ class HTML extends Base
 	public static function sanitizePhoneNumber( $number )
 	{
 		return self::escapeURL( 'tel:'.str_replace( array( '(', ')', '-', '.', '|', ' ' ), '', $number ) );
+	}
+
+	public static function sanitizeSMSNumber( $number )
+	{
+		return self::escapeURL( 'sms:'.str_replace( array( '(', ')', '-', '.', '|', ' ' ), '', $number ) );
 	}
 
 	// FIXME: DEPRECATED
