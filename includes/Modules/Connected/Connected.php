aff6d22494757ce452c7aa8eade08d67ad86172f
Nasser Rafie <contact@geminorum.ir>
2020-07-15 02:37:47 +0430

    module/connected: separeted pot

--- includes/Modules/Connected/Connected.php
+++ includes/Modules/Connected/Connected.php
@@ -8,6 +8,7 @@ class Connected extends gEditorial\Module
 {
 
 	protected $disable_no_posttypes = TRUE;
+	protected $textdomain_frontend  = FALSE;
 
 	public static function module()
 	{
@@ -27,34 +28,34 @@ class Connected extends gEditorial\Module
 			'_general' => [
 				[
 					'field'       => 'admin_column',
-					'title'       => _x( 'Admin Column', 'Modules: Connected: Settings', 'geditorial' ),
-					'description' => _x( 'Displays connected column on admin list screen.', 'Modules: Connected: Settings', 'geditorial' ),
+					'title'       => _x( 'Admin Column', 'Settings', 'geditorial-connected' ),
+					'description' => _x( 'Displays connected column on admin list screen.', 'Settings', 'geditorial-connected' ),
 				],
 				[
 					'field'       => 'admin_box_context',
-					'title'       => _x( 'Admin Box Context', 'Modules: Connected: Setting Title', 'geditorial' ),
-					'description' => _x( 'Where to display the connected box on admin post edit screen.', 'Modules: Connected: Settings', 'geditorial' ),
+					'title'       => _x( 'Admin Box Context', 'Setting Title', 'geditorial-connected' ),
+					'description' => _x( 'Where to display the connected box on admin post edit screen.', 'Settings', 'geditorial-connected' ),
 					'values'      => [
-						_x( 'Normal', 'Modules: Connected: Settings', 'geditorial' ),
-						_x( 'Advanced', 'Modules: Connected: Settings', 'geditorial' ),
+						_x( 'Normal', 'Settings', 'geditorial-connected' ),
+						_x( 'Advanced', 'Settings', 'geditorial-connected' ),
 					],
 				],
 				[
 					'field'       => 'duplicate_connections',
-					'title'       => _x( 'Duplicate Connections', 'Modules: Connected: Settings', 'geditorial' ),
-					'description' => _x( 'Displays multiple connections between connecties.', 'Modules: Connected: Settings', 'geditorial' ),
+					'title'       => _x( 'Duplicate Connections', 'Settings', 'geditorial-connected' ),
+					'description' => _x( 'Displays multiple connections between connecties.', 'Settings', 'geditorial-connected' ),
 				],
 				[
 					'field'       => 'field_desc',
-					'title'       => _x( 'Description Field', 'Modules: Connected: Settings', 'geditorial' ),
-					'description' => _x( 'Displays description field on connected metabox.', 'Modules: Connected: Settings', 'geditorial' ),
+					'title'       => _x( 'Description Field', 'Settings', 'geditorial-connected' ),
+					'description' => _x( 'Displays description field on connected metabox.', 'Settings', 'geditorial-connected' ),
 				],
 				[
 					'field'       => 'string_desc',
 					'type'        => 'text',
-					'title'       => _x( 'Description Title', 'Modules: Connected: Settings', 'geditorial' ),
-					'description' => _x( 'Appears as description column title on connected metabox.', 'Modules: Connected: Settings', 'geditorial' ),
-					'default'     => _x( 'Description', 'Modules: Connected: Settings', 'geditorial' ),
+					'title'       => _x( 'Description Title', 'Settings', 'geditorial-connected' ),
+					'description' => _x( 'Appears as description column title on connected metabox.', 'Settings', 'geditorial-connected' ),
+					'default'     => _x( 'Description', 'Settings', 'geditorial-connected' ),
 				],
 			],
 		];
@@ -86,18 +87,18 @@ class Connected extends gEditorial\Module
 				'context' => $this->get_setting( 'admin_box_context', FALSE ) ? 'advanced' : 'normal',
 			],
 
-			'title'     => _x( 'Connected', 'Modules: Connected', 'geditorial' ),
+			'title'     => _x( 'Connected', 'Metabox Title', 'geditorial-connected' ),
 			'to_labels' => [
-				'singular_name' => _x( 'Connectie', 'Modules: Connected', 'geditorial' ),
-				'search_items'  => _x( 'Search Connecties', 'Modules: Connected', 'geditorial' ),
-				'not_found'     => _x( 'No Connecties found.', 'Modules: Connected', 'geditorial' ),
-				'create'        => _x( 'Connect to a connectie', 'Modules: Connected', 'geditorial' ),
+				'singular_name' => _x( 'Connectie', 'Label', 'geditorial-connected' ),
+				'search_items'  => _x( 'Search Connecties', 'Label', 'geditorial-connected' ),
+				'not_found'     => _x( 'No Connecties found.', 'Label', 'geditorial-connected' ),
+				'create'        => _x( 'Connect to a connectie', 'Label', 'geditorial-connected' ),
 			],
 		];
 
 		if ( $this->get_setting( 'field_desc', FALSE ) )
 			$args['fields']['desc'] = [
-				'title' => $this->get_setting( 'string_desc', _x( 'Description', 'Modules: Connected', 'geditorial' ) ),
+				'title' => $this->get_setting( 'string_desc', _x( 'Description', 'Field Title', 'geditorial-connected' ) ),
 				'type'  => 'text',
 			];
 
