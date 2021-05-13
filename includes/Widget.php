c18ae539f27c7670dabd5c5263b7d32d97bc238d
Nasser Rafie <contact@geminorum.ir>
2021-05-14 00:49:08 +0430

    main/widget: form has thumbnail method

--- includes/Widget.php
+++ includes/Widget.php
@@ -604,4 +604,19 @@ class Widget extends \WP_Widget
 
 		HTML::label( _x( 'Term:', 'Widget Core', 'geditorial' ).$html, $this->get_field_id( $field ) );
 	}
+
+	public function form_has_thumbnail( $instance, $default = FALSE, $field = 'has_thumbnail', $label = NULL )
+	{
+		if ( is_null( $label ) )
+			$label = _x( 'Must has Thumbnail Image', 'Widget Core', 'geditorial' );
+
+		$html = HTML::tag( 'input', [
+			'type'    => 'checkbox',
+			'name'  => $this->get_field_name( $field ),
+			'id'    => $this->get_field_id( $field ),
+			'checked' => isset( $instance[$field] ) ? $instance[$field] : $default,
+		] );
+
+		HTML::label( $html.'&nbsp;'.$label, $this->get_field_id( $field ) );
+	}
 }
