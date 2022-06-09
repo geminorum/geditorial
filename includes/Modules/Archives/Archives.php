6d8944465d9fa25e8770e2ff908ab8970036ec50
Nasser Rafie <contact@geminorum.ir>
2022-06-10 04:14:58 +0430

    module/archives: filtering taxonomy archive links on parent plugin

--- includes/Modules/Archives/Archives.php
+++ includes/Modules/Archives/Archives.php
@@ -107,6 +107,7 @@ class Archives extends gEditorial\Module
 
 		$this->filter_module( 'countables', 'taxonomy_countbox_tokens', 4, 9 );
 
+		$this->filter( 'taxonomy_archive_link', 2, 10, FALSE, 'geditorial' );
 		$this->filter( 'navigation_taxonomy_archive_link', 2, 9, FALSE, 'gtheme' );
 		$this->filter( 'navigation_general_items', 1, 10, FALSE, 'gnetwork' );
 	}
@@ -291,6 +292,14 @@ class Archives extends gEditorial\Module
 			$tokens['link'] = $link;
 
 		return $tokens;
+	}
+
+	public function taxonomy_archive_link( $false, $taxonomy )
+	{
+		if ( $link = $this->get_taxonomy_archive_link( $taxonomy ) )
+			return $link;
+
+		return $false;
 	}
 
 	public function navigation_taxonomy_archive_link( $false, $taxonomy )
