a3c814e8c54592cee7ea3a68fcb90ed15bbb0bdf
Nasser Rafie <contact@geminorum.ir>
2023-08-18 23:00:09 +0330

    internal/frame-page: main-link for term

--- includes/Internals/FramePage.php
+++ includes/Internals/FramePage.php
@@ -63,4 +63,59 @@ trait FramePage
 			], $args['data'] ),
 		], sprintf( $text, Helper::getIcon( $args['icon'] ?? $this->module->icon ), $name ) );
 	}
+
+	protected function framepage_get_mainlink_for_term( $term, $atts = [] )
+	{
+		if ( ! $term = WordPress\Term::get( $term ) )
+			return FALSE;
+
+		$args = self::atts( [
+			'context'      => 'mainbutton',
+			'link_context' => 'framepage',
+			'refkey'       => 'linked',
+			'taxonomy'     => $term->taxonomy,
+			'target'       => 'none',
+			'maxwidth'     => '95%', // '1100px',
+			'link'         => NULL,
+			'name'         => NULL,
+			'title'        => NULL,
+			'text'         => NULL,
+			'icon'         => NULL,
+			'extra'        => [],
+			'data'         => [],
+		], $atts );
+
+		$link = $args['link'] ?? $this->get_adminpage_url( TRUE, [
+			$args['refkey'] => $term->term_id,
+			'target'        => $args['target'] ?? 'none',
+			'noheader'      => 1,
+		], $args['link_context'] ?? 'framepage' );
+
+		$name  = Helper::getTaxonomyLabel( $args['taxonomy'], 'singular_name' );
+		$title = $args['title'] ?? $this->get_string( $args['context'].'_title', $args['taxonomy'], 'metabox', NULL );
+		$text  = $args['text'] ?? $this->get_string( $args['context'].'_text', $args['taxonomy'], 'metabox', $args['name'] ?? $name );
+		$class = [ 'do-colorbox-iframe' ];
+
+		if ( in_array( $args['context'], [ 'mainbutton' ], TRUE ) )
+			$class = array_merge( $class, [
+				'button',
+				'-button',
+				'-button-full',
+				'-button-icon',
+				'-mainbutton',
+			] );
+
+		return Core\HTML::tag( 'a', [
+			'href'   => $link,
+			'title'  => $title ? sprintf( $title, WordPress\Term::title( $term, $name ), $name ) : FALSE,
+			'class'  => array_merge( $class, $args['extra'] ),
+			'target' => '_blank',
+			'data'   => array_merge( [
+				'module'        => $this->key,
+				$args['refkey'] => $term->term_id,
+				'target'        => $args['target'] ?? 'none',
+				'max-width'     => $args['maxwidth'],
+			], $args['data'] ),
+		], sprintf( $text, Helper::getIcon( $args['icon'] ?? $this->module->icon ), $name ) );
+	}
 }
