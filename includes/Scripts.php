04287bd1425f4dd834049bde2ba5b113ab06599e
Nasser Rafie <contact@geminorum.ir>
2023-09-24 18:11:20 +0330

    :up: jquery tree 1.7.3

--- includes/Scripts.php
+++ includes/Scripts.php
@@ -353,7 +353,7 @@ class Scripts extends WordPress\Main
 
 	// @REF: https://github.com/mbraak/jqTree
 	// @REF: http://mbraak.github.io/jqTree/
-	public static function pkgJqTree( $enqueue = FALSE, $ver = '1.7.1' )
+	public static function pkgJqTree( $enqueue = FALSE, $ver = '1.7.3' )
 	{
 		return $enqueue
 			? self::enqueuePackage( 'jqtree', 'jqtree/tree.jquery', [ 'jquery' ], $ver )
