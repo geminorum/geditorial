fb24119dae71617c7e1dcca092a0d5ac0d621502
Nasser Rafie <contact@geminorum.ir>
2023-09-27 21:08:25 +0330

    main/scripts: late check for color-box links

--- assets/js/all.colorbox.js
+++ assets/js/all.colorbox.js
@@ -1,49 +1,51 @@
 (function ($) {
-  $('a.do-colorbox-iframe').each(function () {
-    const instance = $(this);
+  $(window).load(function () {
+    $('a.do-colorbox-iframe').each(function () {
+      const instance = $(this);
 
-    instance.on('click', function (event) {
-      event.preventDefault();
+      instance.on('click', function (event) {
+        event.preventDefault();
 
-      const options = {
-        href: instance.attr('href'),
-        title: instance.attr('title'),
-        iframe: true,
-        fastIframe: false,
-        // closeButton: false,
-        // preloading: false,
-        transition: 'none',
-        width: '95%',
-        height: '85%',
-        maxWidth: instance.data('max-width') || '980',
-        maxHeight: instance.data('max-height') || '640',
-        onClosed: function () {
-          // @REF: https://www.sitepoint.com/jquery-custom-events/
-          $.event.trigger({
-            type: 'gEditorial:ColorBox:Closed',
-            // @REF: https://stackoverflow.com/questions/4187032/get-list-of-data-attributes-using-javascript-jquery
-            passedData: instance.data(),
-            passedLink: instance.attr('href'),
-            passedTitle: instance.attr('title'),
-            colorboxType: 'iframe'
+        const options = {
+          href: instance.attr('href'),
+          title: instance.attr('title'),
+          iframe: true,
+          fastIframe: false,
+          // closeButton: false,
+          // preloading: false,
+          transition: 'none',
+          width: '95%',
+          height: '85%',
+          maxWidth: instance.data('max-width') || '980',
+          maxHeight: instance.data('max-height') || '640',
+          onClosed: function () {
+            // @REF: https://www.sitepoint.com/jquery-custom-events/
+            $.event.trigger({
+              type: 'gEditorial:ColorBox:Closed',
+              // @REF: https://stackoverflow.com/questions/4187032/get-list-of-data-attributes-using-javascript-jquery
+              passedData: instance.data(),
+              passedLink: instance.attr('href'),
+              passedTitle: instance.attr('title'),
+              colorboxType: 'iframe'
+            });
+          }
+        };
+
+        $.colorbox(options);
+
+        $(window).on('resize', function () {
+          $.colorbox.resize({
+            width: window.innerWidth > parseInt(options.maxWidth) ? options.maxWidth : options.width,
+            height: window.innerHeight > parseInt(options.maxHeight) ? options.maxHeight : options.height
           });
-        }
-      };
-
-      $.colorbox(options);
-
-      $(window).on('resize', function () {
-        $.colorbox.resize({
-          width: window.innerWidth > parseInt(options.maxWidth) ? options.maxWidth : options.width,
-          height: window.innerHeight > parseInt(options.maxHeight) ? options.maxHeight : options.height
         });
       });
+
+      // $(document).bind('cbox_closed', function () {
+      //   console.log($.colorbox.title);
+      // });
+
+      // $.colorbox.close();
     });
-
-    // $(document).bind('cbox_closed', function () {
-    //   console.log($.colorbox.title);
-    // });
-
-    // $.colorbox.close();
   });
 })(jQuery);
