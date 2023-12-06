7396d25b10185ec89510f8cd12747c6178429c71
Nasser Rafie <contact@geminorum.ir>
2023-12-06 19:12:08 +0330

    module/persona: reverse name family on full-name

--- includes/Modules/Persona/ModuleHelper.php
+++ includes/Modules/Persona/ModuleHelper.php
@@ -4,7 +4,6 @@ defined( 'ABSPATH' ) || die( header( 'HTTP/1.0 403 Forbidden' ) );
 
 use geminorum\gEditorial;
 use geminorum\gEditorial\Core;
-use geminorum\gEditorial\Core\Arraay;
 use geminorum\gEditorial\WordPress;
 
 class ModuleHelper extends gEditorial\Helper
@@ -13,14 +12,33 @@ class ModuleHelper extends gEditorial\Helper
 	const MODULE = 'persona';
 
 	// TODO: support more complex naming
-	public static function makeFullname( $names, $fallback = FALSE )
+	public static function makeFullname( $data, $fallback = FALSE )
 	{
-		if ( ! $names )
+		if ( ! $data )
 			return $fallback;
 
-		if ( empty( $names['last_name'] ) && empty( $names['first_name'] ) )
-			return $fallback;
+		$parts = self::atts( [
+			'fullname'    => '',
+			'first_name'  => '',
+			'last_name'   => '',
+			'middle_name' => '',
+			'father_name' => '',
+			'mother_name' => '',
+		], $data );
 
-		return sprintf( '%s %s', $names['first_name'], $names['last_name'] );
+		if ( empty( $parts['last_name'] ) && empty( $parts['first_name'] ) )
+			return empty( $parts['fullname'] ) ? $fallback : $parts['fullname'];
+
+		return Core\Text::normalizeWhitespace( vsprintf(
+			/* translators: %1$s: first name, %2$s: last name, %3$s: middle name, %4$s: father name, %5$s: mother name */
+			_x( '%2$s, %1$s %3$s', 'Helper: Make Full-name', 'geditorial-persona' ),
+			[
+				$parts['first_name'],
+				$parts['last_name'],
+				$parts['middle_name'],
+				$parts['father_name'],
+				$parts['mother_name'],
+			]
+			), FALSE );
 	}
 }
