db8cdaa31534607bf6aaabd290f4abb1e8ab9523
Nasser Rafie <contact@geminorum.ir>
2023-12-07 21:35:12 +0330

    module/executed: :new: module

---
+++ includes/Modules/Executed/Executed.php
@@ -0,0 +1,142 @@
+<?php namespace geminorum\gEditorial\Modules\Executed;
+
+defined( 'ABSPATH' ) || die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+use geminorum\gEditorial;
+use geminorum\gEditorial\Core;
+use geminorum\gEditorial\Internals;
+use geminorum\gEditorial\WordPress;
+
+class Executed extends gEditorial\Module
+{
+	use Internals\CoreCapabilities;
+	use Internals\CoreDashboard;
+	use Internals\CoreMenuPage;
+	use Internals\CoreRestrictPosts;
+	use Internals\DashboardSummary;
+
+	protected $disable_no_posttypes = TRUE;
+
+	public static function module()
+	{
+		return [
+			'name'   => 'executed',
+			'title'  => _x( 'Executed', 'Modules: Executed', 'geditorial-admin' ),
+			'desc'   => _x( 'Editorial Executions', 'Modules: Executed', 'geditorial-admin' ),
+			'icon'   => 'hammer',
+			'access' => 'beta',
+		];
+	}
+
+	protected function get_global_settings()
+	{
+		$terms = WordPress\Taxonomy::listTerms( $this->constant( 'main_taxonomy' ) );
+		$empty = $this->get_taxonomy_label( 'main_taxonomy', 'no_items_available', NULL, 'no_terms' );
+
+		return [
+			'posttypes_option' => 'posttypes_option',
+			'_roles'     => $this->corecaps_taxonomy_get_roles_settings( 'main_taxonomy', TRUE, TRUE, $terms, $empty ),
+			'_dashboard' => [
+				'dashboard_widgets',
+				'summary_excludes' => [ NULL, $terms, $empty ],
+				'summary_scope',
+				'summary_drafts',
+				'count_not',
+			],
+		];
+	}
+
+	protected function get_global_constants()
+	{
+		return [
+			'main_taxonomy' => 'executive',
+		];
+	}
+
+	protected function get_module_icons()
+	{
+		return [
+			'taxonomies' => [
+				'main_taxonomy' => NULL,
+			],
+		];
+	}
+
+	protected function get_global_strings()
+	{
+		$strings = [
+			'noops' => [
+				'main_taxonomy' => _n_noop( 'Executive', 'Executives', 'geditorial-executed' ),
+			],
+			'labels' => [
+				'main_taxonomy' => [
+					'show_option_all'      => _x( 'Executives', 'Label: Show Option All', 'geditorial-executed' ),
+					'show_option_no_items' => _x( '(Unexecutived)', 'Label: Show Option No Terms', 'geditorial-executed' ),
+				],
+			],
+		];
+
+		if ( ! is_admin() )
+			return $strings;
+
+		$strings['dashboard'] = [
+			'current' => [ 'widget_title' => _x( 'Your Team Execution Summary', 'Dashboard Widget Title', 'geditorial-executed' ), ],
+			'all'     => [ 'widget_title' => _x( 'Editorial Execution Summary', 'Dashboard Widget Title', 'geditorial-executed' ), ],
+		];
+
+		$strings['default_terms'] = [
+			'main_taxonomy' => [
+				// 'illiterate' => _x( 'Illiterate', 'Default Term', 'geditorial-executed' ),
+			],
+		];
+
+		return $strings;
+	}
+
+	public function init()
+	{
+		parent::init();
+
+		$this->register_taxonomy( 'main_taxonomy', [
+			'hierarchical' => TRUE,
+			'show_in_menu' => FALSE,
+			'meta_box_cb'  => '__checklist_restricted_terms_callback',
+		], NULL, TRUE );
+
+		$this->corecaps__handle_taxonomy_metacaps_roles( 'main_taxonomy' );
+	}
+
+	public function current_screen( $screen )
+	{
+		if ( $this->constant( 'main_taxonomy' ) == $screen->taxonomy ) {
+
+			$this->filter_string( 'parent_file', 'options-general.php' );
+
+		} else if ( $this->posttype_supported( $screen->post_type ) ) {
+
+			if ( 'edit' == $screen->base ) {
+
+				if ( $this->corecaps_taxonomy_role_can( 'main_taxonomy', 'reports' ) )
+					$this->corerestrictposts__hook_screen_taxonomies( 'main_taxonomy' );
+			}
+		}
+	}
+
+	public function admin_menu()
+	{
+		$this->_hook_menu_taxonomy( 'main_taxonomy', 'options-general.php' );
+	}
+
+	protected function dashboard_widgets()
+	{
+		if ( ! $this->corecaps_taxonomy_role_can( 'main_taxonomy', 'reports' ) )
+			return;
+
+		$this->add_dashboard_widget( 'term-summary', NULL, 'refresh' );
+	}
+
+	public function render_widget_term_summary( $object, $box )
+	{
+		$this->do_dashboard_term_summary( 'main_taxonomy', $box );
+	}
+}
