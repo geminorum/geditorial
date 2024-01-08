250528ef0470290cd9024b3a08ed82fb14efd5b0
Nasser Rafie <contact@geminorum.ir>
2024-01-08 16:03:06 +0330

    asset/color-box: migrate to module structure script

--- assets/js/all.colorbox.js
+++ assets/js/all.colorbox.js
@@ -1,51 +1,70 @@
-(function ($) {
-  $(window).load(function () {
-    $('a.do-colorbox-iframe').each(function () {
-      const instance = $(this);
+(function ($, plugin, module, section) {
+  const s = {
+    // action: plugin._base + '_' + module,
+    // classs: plugin._base + '-' + module,
+    before: 'do-' + module + '-iframe',
+    after: 'hooked-' + module + '-iframe'
+  };
 
-      instance.on('click', function (event) {
-        event.preventDefault();
+  const app = {
+    hook: function () {
+      $('a.' + s.before).each(function () {
+        const $instance = $(this);
 
-        const options = {
-          href: instance.attr('href'),
-          title: instance.attr('title'),
-          iframe: true,
-          fastIframe: false,
-          // closeButton: false,
-          // preloading: false,
-          transition: 'none',
-          width: '95%',
-          height: '85%',
-          maxWidth: instance.data('max-width') || '980',
-          maxHeight: instance.data('max-height') || '640',
-          onClosed: function () {
-            // @REF: https://www.sitepoint.com/jquery-custom-events/
-            $.event.trigger({
-              type: 'gEditorial:ColorBox:Closed',
-              // @REF: https://stackoverflow.com/questions/4187032/get-list-of-data-attributes-using-javascript-jquery
-              passedData: instance.data(),
-              passedLink: instance.attr('href'),
-              passedTitle: instance.attr('title'),
-              colorboxType: 'iframe'
+        $instance.on('click', function (event) {
+          event.preventDefault();
+
+          const options = {
+            href: $instance.attr('href'),
+            title: $instance.attr('title'),
+            iframe: true,
+            fastIframe: false,
+            // closeButton: false,
+            // preloading: false,
+            transition: 'none',
+            width: '95%',
+            height: '85%',
+            maxWidth: $instance.data('max-width') || '980',
+            maxHeight: $instance.data('max-height') || '640',
+            onClosed: function () {
+              // @REF: https://www.sitepoint.com/jquery-custom-events/
+              $.event.trigger({
+                type: 'gEditorial:ColorBox:Closed',
+                // @REF: https://stackoverflow.com/questions/4187032/get-list-of-data-attributes-using-javascript-jquery
+                passedData: $instance.data(),
+                passedLink: $instance.attr('href'),
+                passedTitle: $instance.attr('title'),
+                colorboxType: 'iframe'
+              });
+            }
+          };
+
+          $.colorbox(options);
+
+          $(window).on('resize', function () {
+            $.colorbox.resize({
+              width: window.innerWidth > parseInt(options.maxWidth) ? options.maxWidth : options.width,
+              height: window.innerHeight > parseInt(options.maxHeight) ? options.maxHeight : options.height
             });
-          }
-        };
-
-        $.colorbox(options);
-
-        $(window).on('resize', function () {
-          $.colorbox.resize({
-            width: window.innerWidth > parseInt(options.maxWidth) ? options.maxWidth : options.width,
-            height: window.innerHeight > parseInt(options.maxHeight) ? options.maxHeight : options.height
           });
         });
+
+        $instance.removeClass(s.before);
+        $instance.addClass(s.after);
+
+        // $(document).bind('cbox_closed', function () {
+        //   console.log($.colorbox.title);
+        // });
+
+        // $.colorbox.close();
       });
+    }
+  };
 
-      // $(document).bind('cbox_closed', function () {
-      //   console.log($.colorbox.title);
-      // });
+  $(window).load(function () {
+    app.hook();
 
-      // $.colorbox.close();
-    });
+    $(document).on('gEditorial:ColorBox:Hook', function () { app.hook(); });
+    $(document).trigger('gEditorial:Module:Loaded', [module, app]);
   });
-})(jQuery);
+}(jQuery, gEditorial, 'colorbox', 'all'));
