6e616f099f56095342b13e2382a2f2ef28d9a35b
Nasser Rafie <contact@geminorum.ir>
2024-06-22 02:51:12 +0330

    module/housed: :new: module

---
+++ includes/Modules/Housed/Housed.php
@@ -0,0 +1,186 @@
+<?php namespace geminorum\gEditorial\Modules\Housed;
+
+defined( 'ABSPATH' ) || die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+use geminorum\gEditorial;
+use geminorum\gEditorial\Core;
+use geminorum\gEditorial\Internals;
+use geminorum\gEditorial\WordPress;
+
+class Housed extends gEditorial\Module
+{
+	use Internals\CoreCapabilities;
+	use Internals\CoreDashboard;
+	use Internals\CoreMenuPage;
+	use Internals\CoreRestrictPosts;
+	use Internals\DashboardSummary;
+	use Internals\TemplateTaxonomy;
+
+	protected $disable_no_posttypes = TRUE;
+
+	public static function module()
+	{
+		return [
+			'name'     => 'housed',
+			'title'    => _x( 'Housed', 'Modules: Housed', 'geditorial-admin' ),
+			'desc'     => _x( 'Content Housings', 'Modules: Housed', 'geditorial-admin' ),
+			'icon'     => 'admin-home',
+			'access'   => 'beta',
+			'keywords' => [
+				'housing',
+				'taxmodule',
+			],
+		];
+	}
+
+	protected function get_global_settings()
+	{
+		$terms = WordPress\Taxonomy::listTerms( $this->constant( 'main_taxonomy' ) );
+		$empty = $this->get_taxonomy_label( 'main_taxonomy', 'no_items_available', NULL, 'no_terms' );
+
+		return [
+			'posttypes_option' => 'posttypes_option',
+			'_roles'     => $this->corecaps_taxonomy_get_roles_settings( 'main_taxonomy', TRUE, TRUE, $terms, $empty ),
+			'_dashboard' => [
+				'dashboard_widgets',
+				'summary_excludes' => [ NULL, $terms, $empty ],
+				'summary_scope',
+				'summary_drafts',
+				'count_not',
+			],
+			'_editlist' => [
+				'show_in_quickedit',
+			],
+			'_frontend' => [
+				'contents_viewable',
+				'show_in_navmenus',
+			],
+		];
+	}
+
+	protected function get_global_constants()
+	{
+		return [
+			'main_taxonomy' => 'housing',
+		];
+	}
+
+	protected function get_module_icons()
+	{
+		return [
+			'taxonomies' => [
+				'main_taxonomy' => NULL,
+			],
+		];
+	}
+
+	protected function get_global_strings()
+	{
+		$strings = [
+			'noops' => [
+				'main_taxonomy' => _n_noop( 'Housing Condition', 'Housing Conditions', 'geditorial-housed' ),
+			],
+			'labels' => [
+				'main_taxonomy' => [
+					'menu_name'            => _x( 'Housing Conditions', 'Label: `menu_name`', 'geditorial-housed' ),
+					'extended_label'       => _x( 'Housing Conditions', 'Label: `extended_label`', 'geditorial-housed' ),
+					'show_option_all'      => _x( 'Housings', 'Label: `show_option_all`', 'geditorial-housed' ),
+					'show_option_no_items' => _x( '(Undefined)', 'Label: `show_option_no_items`', 'geditorial-housed' ),
+				],
+			],
+		];
+
+		return $strings;
+	}
+
+	protected function define_default_terms()
+	{
+		return [
+			'main_taxonomy' => [
+				'ownership' => _x( 'Ownership', 'Main Taxonomy: Default Term', 'geditorial-housed' ),
+				'rental'    => _x( 'Rental', 'Main Taxonomy: Default Term', 'geditorial-housed' ),
+				'parental'  => _x( 'Parental', 'Main Taxonomy: Default Term', 'geditorial-housed' ),
+				'dormitory' => _x( 'Dormitory', 'Main Taxonomy: Default Term', 'geditorial-housed' ),
+				'homeless'  => _x( 'Homeless', 'Main Taxonomy: Default Term', 'geditorial-housed' ),
+			],
+		];
+	}
+
+	public function init()
+	{
+		parent::init();
+
+		$this->register_taxonomy( 'main_taxonomy', [
+			'hierarchical'       => TRUE,
+			'show_in_menu'       => FALSE,
+			'show_in_quick_edit' => (bool) $this->get_setting( 'show_in_quickedit' ),
+			'show_in_nav_menus'  => (bool) $this->get_setting( 'show_in_navmenus' ),
+		], NULL, [
+			'is_viewable'    => $this->get_setting( 'contents_viewable', TRUE ),
+			'custom_captype' => TRUE,
+		] );
+
+		$this->corecaps__handle_taxonomy_metacaps_roles( 'main_taxonomy' );
+		$this->hook_taxonomy_importer_term_singleselect( $this->constant( 'main_taxonomy' ), TRUE );
+	}
+
+	public function current_screen( $screen )
+	{
+		if ( $this->constant( 'main_taxonomy' ) == $screen->taxonomy ) {
+
+			$this->filter_string( 'parent_file', 'options-general.php' );
+
+		} else if ( $this->posttype_supported( $screen->post_type ) ) {
+
+			if ( 'edit' == $screen->base ) {
+
+				if ( $this->corecaps_taxonomy_role_can( 'main_taxonomy', 'reports' ) )
+					$this->corerestrictposts__hook_screen_taxonomies( 'main_taxonomy' );
+
+			} else if ( 'post' === $screen->base ) {
+
+				$this->hook_taxonomy_metabox_mainbox(
+					'main_taxonomy',
+					$screen->post_type,
+					'__singleselect_restricted_terms_callback'
+				);
+			}
+		}
+	}
+
+	public function admin_menu()
+	{
+		$this->_hook_menu_taxonomy( 'main_taxonomy', 'options-general.php' );
+	}
+
+	public function dashboard_widgets()
+	{
+		if ( ! $this->corecaps_taxonomy_role_can( 'main_taxonomy', 'reports' ) )
+			return;
+
+		$this->add_dashboard_widget(
+			'term-summary',
+			$this->get_taxonomy_label( 'main_taxonomy', 'extended_label' ),
+			'refresh'
+		);
+	}
+
+	public function render_widget_term_summary( $object, $box )
+	{
+		$this->do_dashboard_term_summary( 'main_taxonomy', $box );
+	}
+
+	public function cuc( $context = 'settings', $fallback = '' )
+	{
+		return 'reports' == $context
+			? $this->corecaps_taxonomy_role_can( 'main_taxonomy', 'reports', NULL, $fallback )
+			: parent::cuc( $context, $fallback );
+	}
+
+	public function template_include( $template )
+	{
+		return $this->get_setting( 'contents_viewable', TRUE )
+			? $this->templatetaxonomy__include( $template, $this->constant( 'main_taxonomy' ) )
+			: $template;
+	}
+}
