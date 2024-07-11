88fc76a33592a42d52cc99fdd98adfe0cff6af62
Nasser Rafie <contact@geminorum.ir>
2024-07-11 19:53:35 +0330

    module/next-of-kin: app updated

--- assets/apps/family-grid/build/main.asset.php
+++ assets/apps/family-grid/build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch'), 'version' => '1d128dbdaa3a17f66a9c');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-i18n'), 'version' => '94d763c4577b0dbbf6c0');
--- assets/apps/family-grid/src/App.vue
+++ assets/apps/family-grid/src/App.vue
@@ -7,25 +7,33 @@
     <table class="table table-striped table-bordered app-table">
       <thead>
         <tr>
+          <th class="index">{{ i18n.index }}</th>
           <th v-for="(field, key) in fields" :class="['field-'+key, { required: config.required.includes(key) }]">{{ field }}</th>
-          <th class="actions">{{ $translate('actions') }}</th>
+          <th class="actions">
+            {{ $translate('actions') }}
+            <GridSpinner :class="{ 'is-active': spinner }" :title="i18n.loading"></GridSpinner>
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in items" :key="item._id" :class="{ 'is-editing': editing == item._id }">
+        <tr v-for="(item, index) in items" :key="item._id" :class="{ 'is-editing': editing == item._id }">
+          <td class="index">{{ cellIndex(index) }}</td>
           <td v-for="(field, key) in fields" :title="field" :class="['field-'+key, cellClass(item[key], key)]">{{item[key]}}</td>
-          <td class="actions">
+          <td class="actions"><div>
             <GridButton @click="removeItem(item._id)" dashicon="remove" :title="i18n.remove" />
             <GridPopper>
               <GridButton @click="infoItem(item._id)" dashicon="info" :title="i18n.info" />
               <template #content>
-                <div>{{ infoContent(item._id) }}</div>
+                <GridInfo :info="info" :item="item" />
               </template>
             </GridPopper>
             <GridButton @click="editItem(item._id)" :dashicon="editing == item._id ? 'dismiss' : 'edit'" :title="i18n.edit" :class="{ 'is-editing': editing == item._id }" />
-          </td>
+            <GridButton @click="moveItem(index, true)" dashicon="arrow-up" :title="i18n.moveup" />
+            <GridButton @click="moveItem(index, false)" dashicon="arrow-down" :title="i18n.movedown" />
+          </div></td>
         </tr>
         <tr>
+          <td class="plus">{{ i18n.plus }}</td>
           <td v-for="(field, key) in fields" :class="['form', { 'is-not-valid': isNotValidInput(key) }]">
             <GridInput
               :field="key"
@@ -35,28 +43,22 @@
               @change="event => updateInput(key, event.target.value)"
             />
           </td>
-          <td class="actions">
+          <td class="actions"><div>
             <GridButton @click="insertItem()" dashicon="insert" :title="i18n.insert" />
-            <GridButton @click="infoItem(0)" dashicon="info" :title="i18n.info" />
-            <GridSpinner :class="{ 'is-active': spinner }" :title="i18n.loading"></GridSpinner>
-          </td>
+            </div></td>
         </tr>
       </tbody>
     </table>
-    <GridMessage :class="state" :message="message"></GridMessage>
+    <GridMessage :class="state" :message="message" :count="items.length"></GridMessage>
   </div>
 </template>
 
 <script>
 import { debounce } from 'lodash-es';
 import { EnterToTabMixin } from '../../vue-plugins/vue-enter-to-tab.v2'; // https://github.com/ajomuch92/vue-enter-to-tab
-import { verifyDateString, verifyYearString } from '../../js-utils/datetime.v1';
-import { checkMobileByLocale } from '../../js-utils/mobile.v1';
-
-// https://github.com/WordPress/gutenberg/tree/trunk/packages/api-fetch
+import { formatNumber } from '../../js-utils/number.v1';
+import { isValidField } from '../../js-utils/fields.v1';
 import apiFetch from '@wordpress/api-fetch'; // https://developer.wordpress.org/block-editor/reference-guides/packages/packages-api-fetch/
-// import { addQueryArgs } from '@wordpress/url'; // https://github.com/WordPress/gutenberg/tree/trunk/packages/url
-import { verifyIranianNationalId } from "@persian-tools/persian-tools"; // https://github.com/persian-tools/persian-tools
 
 export default {
   mixins: [EnterToTabMixin],
@@ -68,7 +70,11 @@ export default {
       state: 'initial',
       editing: 0,
       items: [],
-      hidden: {},
+      info: {}, // info data fetched
+      current: 0, // current comment id for info
+      hidden: {}, // // list of hidden fields
+      unique: {}, // list of unique fields
+      required: {}, // list of required fields
       form: {}
     }
   },
@@ -78,6 +84,9 @@ export default {
     formReset() {
       // https://stackoverflow.com/a/71344289
       this.form = Object.fromEntries(Object.keys(this.fields).map(key => [key, '']));
+      this.hidden = this.config.hidden || {};
+      this.unique = this.config.unique || {};
+      this.required = this.config.required || {};
       this.editing = 0;
     },
 
@@ -86,16 +95,8 @@ export default {
       this.state = 'initial';
     },
 
-    isDateField(field) {
-      return [
-        'date',
-        'datetime',
-        'datestart',
-        'dateend',
-        'date_of_birth',
-        'date_of_death',
-        'dob'
-      ].includes(field);
+    cellIndex (index) {
+      return formatNumber(index+1, this.locale);
     },
 
     // NOTE: better to be in `methods`: “You can pass a parameter to a computed
@@ -103,24 +104,10 @@ export default {
     // benefits of using a computed property function over a method.”
     // @REF: https://beginnersoftwaredeveloper.com/can-i-pass-a-parameter-to-a-computed-property-vue/
     cellClass(cell, offset) {
-      if(!cell) {
-
-        return '-empty';
-
-      } else if (this.isDateField(offset)) {
-
-        return 'fa-IR'===this.locale && ( verifyDateString(cell) || verifyYearString(cell) ) ? '-is-valid' : '-is-not-valid';
-
-      } else if (offset=='identity'||offset=='identifier') {
-
-        return 'fa-IR'===this.locale && verifyIranianNationalId(cell) ? '-is-valid' : '-is-not-valid';
-
-      } else if (offset=='mobile'||offset=='phone') {
-
-        return checkMobileByLocale(cell, this.locale) ? '-is-valid' : '-is-not-valid';
-      }
-
-      return '-unknown';
+      if (!cell) return '-empty';
+      const valid = isValidField(cell, offset, this.locale);
+      if (valid===null) return '-unknown';
+      return valid ? '-is-valid' : '-is-not-valid';
     },
 
     updateInput(field, value) {
@@ -130,22 +117,29 @@ export default {
     isNotValidInput(field) {
       if(!field) return false;
       if(!this.form[field]||!this.form[field].trim()) return false;
-
-      // if (field === 'iban') {
-      //   return ! isShebaValid(this.form[field]);
-      // } else if (field === 'card') {
-      //   return ! verifyCardNumber(this.form[field]);
-      // }
-
-      return false;
+      const valid = isValidField(this.form[field], field, this.locale);
+      return valid===null ? false : ! valid;
     },
 
     hasValidInput() {
-      for (const required of this.config.required) {
+
+      for (const required of this.required) {
         if (!this.form[required].trim()) {
           return false;
         }
       }
+
+      for (const unique of this.unique) {
+        const current = this.form[unique].trim();
+        if (current) {
+          for (const item of this.items) {
+            if(current===item[unique]) {
+              return false;
+            }
+          }
+        }
+      }
+
       return true;
     },
 
@@ -168,20 +162,53 @@ export default {
       });
     },
 
-    insertItem () {
+    doSort() {
+      this.spinner = true;
 
-      // TODO: check for duplicate rows
+      apiFetch({
+          path: this.endpoint + '/sort/' + this.config.linked,
+          method: 'POST',
+          data: this.items.map( item => item._id )
+        }).then((data) => {
+          this.items = data;
+          this.spinner = false;
+          this.state = 'saved';
+          this.message = this.i18n.sorted;
+          this.formReset();
+        }).catch((error) => {
+          this.spinner = false;
+          this.state = 'wrong';
+          this.message = error.message;
+        });
+    },
+
+    doInfo(id) {
+      if (id === this.current) return;
+
+      this.spinner = true;
+
+      apiFetch({
+        path: this.endpoint + '/summary/' + id
+      }).then((data) => {
+        this.info = data;
+        this.current = id;
+        this.spinner = false;
+      }).catch((error) => {
+        this.spinner = false;
+        this.state = 'wrong';
+        this.message = error.message;
+      });
+    },
+
+    insertItem () {
 
       if (this.hasValidInput()) {
         this.spinner = true;
 
-        const post = this.form;
-        post._id = this.editing;
-
         apiFetch({
           path: this.endpoint + '/query/' + this.config.linked,
           method: 'POST',
-          data: post
+          data: this.form,
         }).then((data) => {
           this.items = data;
           this.spinner = false;
@@ -200,11 +227,8 @@ export default {
     },
 
     infoItem (id) {
-      console.log(id);
-    },
-
-    infoContent (id) {
-      return '+'+id;
+      if (id !== this.current) this.info = {}; // clear the old data
+      this.doInfo(id);
     },
 
     editItem (id) {
@@ -221,6 +245,19 @@ export default {
         }
       }
       this.spinner = false;
+    },
+
+    moveItem (index, direction) {
+      const to = direction ? (index - 1) : (index + 1);
+      if ( to < 0 || to >= this.items.length) {
+        return;
+      }
+
+      const temp = this.items[index];
+      this.items[index] = this.items[to];
+      this.items[to] = temp;
+
+      this.doSort();
     },
 
     removeItem (id) {
@@ -242,19 +279,16 @@ export default {
     },
   },
   created() {
-    // https://vuejs.org/guide/essentials/reactivity-fundamentals.html#stateful-methods
-    // this.debouncedDoQuery = window.lodash.debounce(this.doQuery, 500);
-    // this.debouncedDoQuery = _.debounce(this.doQuery, 500);
-    this.debouncedDoQuery = debounce(this.doQuery, 500);
+    // NOTE: replace intended method with debounced one
+    // @REF: https://stackoverflow.com/a/75374781
+    this.doSort = debounce(this.doSort, 1000);
   },
   unmounted() {
-    this.debouncedDoQuery.cancel();
+	this.doSort.cancel();
   },
   mounted() {
     this.formReset();
     this.$nextTick(() => {
-      // this.fetchLinked();
-      // this.fetchStored();
       this.doQuery();
     });
   }
