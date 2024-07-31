aeb0248c5d319619605a2b03039f97eedf35200c
Nasser Rafie <contact@geminorum.ir>
2024-07-31 23:13:56 +0330

    :up: jqtree 1.8.4

--- includes/Scripts.php
+++ includes/Scripts.php
@@ -479,7 +479,7 @@ class Scripts extends WordPress\Main
 
 	// @REF: https://github.com/mbraak/jqTree
 	// @REF: http://mbraak.github.io/jqTree/
-	public static function pkgJqTree( $enqueue = FALSE, $ver = '1.8.3' )
+	public static function pkgJqTree( $enqueue = FALSE, $ver = '1.8.4' )
 	{
 		return $enqueue
 			? self::enqueuePackage( 'jqtree', 'jqtree/tree.jquery', [ 'jquery' ], $ver )
