f3caa93f8322285fc11a802d2bff70efbeaa436f
Nasser Rafie <contact@geminorum.ir>
2024-08-04 04:42:15 +0330

    :wrench: apps updated

--- assets/apps/subcontent-grid/build/main.asset.php
+++ assets/apps/subcontent-grid/build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-i18n', 'wp-url'), 'version' => '9597e81cd3449157c2a7');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-i18n', 'wp-url'), 'version' => '6830642348f0efcc3f6b');
