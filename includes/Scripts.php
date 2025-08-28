0d8d4d4224b38db22129140a1529584b7777d1ea
Nasser Rafie <contact@geminorum.ir>
2025-08-28 22:00:57 +0330

    🆙 bootstrap 5.3.8

--- includes/Scripts.php
+++ includes/Scripts.php
@@ -709,7 +709,7 @@ JS;
 			: self::registerPackage( 'select2', 'select2/select2', [ 'jquery' ], $ver );
 	}
 
-	public static function linkBootstrap5( $ver = '5.3.7', $screen = 'all' )
+	public static function linkBootstrap5( $ver = '5.3.8', $screen = 'all' )
 	{
 		$var = self::const( 'SCRIPT_DEBUG' ) ? '' : '.min';
 		$dir = Core\HTML::rtl() ? '.rtl' : '';
