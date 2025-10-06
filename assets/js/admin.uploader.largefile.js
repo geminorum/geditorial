c433bd60db1533a5e473ae25144c493ab741bf19
Nasser Rafie <contact@geminorum.ir>
2025-10-06 05:14:46 +0330

    module/uploader: 🆕 module

---
+++ assets/js/admin.uploader.largefile.js
@@ -0,0 +1,199 @@
+/* global FileReader */
+
+(function ($, plugin, module, section) {
+  const sliceSize = 1000 * 1024;
+  let reader = {};
+  let file = {};
+
+  const s = {
+    action: [plugin._base, module].join('_'),
+    classs: [plugin._base, module].join('-'),
+    input: '#' + [plugin._base, module, section, 'input'].join('-'),
+    name: '#' + [plugin._base, module, section, 'name'].join('-'),
+    submit: '#' + [plugin._base, module, section, 'submit'].join('-'),
+    progress: '#' + [plugin._base, module, section, 'progress'].join('-')
+  };
+
+  const app = {
+    check: function () {
+      const $submit = $(s.submit);
+      const $spinner = $(s.progress).prev('.spinner');
+
+      $.ajax({
+        url: plugin._url,
+        type: 'POST',
+        dataType: 'json',
+        cache: false,
+        data: {
+          action: s.action,
+          what: 'upload_check',
+          file: file.name,
+          mime: file.type,
+          nonce: $submit.data('nonce')
+        },
+        beforeSend: function (xhr) {
+          $spinner.addClass('is-active');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR, textStatus, errorThrown);
+        },
+        success: function (response) {
+          $spinner.removeClass('is-active');
+          if (response.success) {
+            app.upload(0);
+          } else {
+            u.io(s.progress, response.data);
+            $submit.prop('disabled', true);
+            console.log(response);
+          }
+        }
+      });
+    },
+
+    upload: function (start) {
+      const sliceNext = start + sliceSize + 1;
+      const blob = file.slice(start, sliceNext);
+
+      reader.onloadend = function (event) {
+        if (event.target.readyState !== FileReader.DONE) {
+          return;
+        }
+
+        const $submit = $(s.submit);
+
+        $.ajax({
+          url: plugin._url,
+          type: 'POST',
+          dataType: 'json',
+          cache: false,
+          data: {
+            action: s.action,
+            what: 'upload_chaunk',
+            file_data: event.target.result,
+            file: file.name,
+            mime: file.type,
+            chunk: start,
+            nonce: $submit.data('nonce')
+          },
+          error: function (jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+          },
+          success: function (response) {
+            if (response.success) {
+              const doneSize = start + sliceSize;
+              let donePercent = Math.floor((doneSize / file.size) * 100);
+              if (sliceNext < file.size) {
+                if ($submit.data('locale') === 'fa_IR') {
+                  donePercent = u.tP(donePercent);
+                }
+                u.io(s.progress, u.sp($submit.data('progress'), donePercent));
+                app.upload(sliceNext);
+              } else {
+                $(s.name).html('').hide();
+                $(s.submit).prop('disabled', true);
+                u.io(s.progress, $submit.data('complete'));
+                app.complete(file.name);
+              }
+            } else {
+              u.io(s.progress, response.data);
+              console.log(response);
+            }
+          }
+        });
+      };
+
+      reader.readAsDataURL(blob);
+    },
+
+    complete: function (filename) {
+      const $submit = $(s.submit);
+      const $spinner = $(s.progress).prev('.spinner');
+
+      $.ajax({
+        url: plugin._url,
+        type: 'GET',
+        data: {
+          action: s.action,
+          what: 'upload_complete',
+          file: filename,
+          nonce: $submit.data('nonce')
+        },
+        beforeSend: function (xhr) {
+          $spinner.addClass('is-active');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR, textStatus, errorThrown);
+        },
+        success: function (response) {
+          $spinner.removeClass('is-active');
+          if (response.success) {
+            u.io(s.progress, response.data);
+          } else {
+            u.io(s.progress, response.data);
+            console.log(response);
+          }
+        }
+      });
+    }
+  };
+
+  const u = {
+    baseName: function (path) {
+      return path.split(/[\\/]/).pop();
+    },
+
+    formatBytes: function (bytes, decimals) {
+      if (bytes === 0) return '0 Bytes';
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+      const i = Math.floor(Math.log(bytes) / Math.log(1024));
+      return parseFloat((bytes / Math.pow(1024, i)).toFixed(decimals || 2)) + ' ' + sizes[i];
+    },
+
+    // @REF: https://gist.github.com/geminorum/ebb48ff0c0df3876e58610dbb5a60f0f
+    sp: function (f) {
+      const a = Array.prototype.slice.call(arguments, 1);
+      let i = 0;
+      return f.replace(/%s/g, function () {
+        return a[i++];
+      });
+    },
+    tP: function (n) {
+      const p = '۰'.charCodeAt(0);
+      return n.toString().replace(/\d+/g, function (m) {
+        return m.split('').map(function (n) {
+          return String.fromCharCode(p + parseInt(n));
+        }).join('');
+      });
+    },
+    io: function (s, h) {
+      $(s).fadeOut('fast', function () {
+        $(this).html(h).fadeIn();
+      });
+    }
+  };
+
+  $(function () {
+    $(s.input).on('change', function () {
+      const filename = $(this).val();
+      if (filename !== '') {
+        $(s.name).html(u.baseName(filename) + ' (' + u.formatBytes(this.files[0].size) + ')').show();
+        $(s.submit).prop('disabled', false);
+      } else {
+        $(s.name).html('').hide();
+        $(s.submit).prop('disabled', true);
+      }
+    });
+
+    $(s.submit).on('click', function (event) {
+      event.preventDefault();
+      $(this).prop('disabled', true);
+
+      reader = new FileReader();
+      file = document.querySelector(s.input).files[0];
+
+      app.check();
+    });
+
+    $(document).trigger('gEditorial:Module:Loaded', [module, app]);
+  });
+}(jQuery, gEditorial, 'uploader', 'largefile'));
---
+++ includes/Modules/Uploader/Uploader.php
@@ -0,0 +1,264 @@
+<?php namespace geminorum\gEditorial\Modules\Uploader;
+
+defined( 'ABSPATH' ) || die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+use geminorum\gEditorial;
+use geminorum\gEditorial\Ajax;
+use geminorum\gEditorial\Core;
+use geminorum\gEditorial\Internals;
+use geminorum\gEditorial\Services;
+use geminorum\gEditorial\WordPress;
+
+class Uploader extends gEditorial\Module
+{
+	use Internals\CoreDashboard;
+
+	public static function module()
+	{
+		return [
+			'name'     => 'uploader',
+			'title'    => _x( 'Uploader', 'Modules: Uploader', 'geditorial-admin' ),
+			'desc'     => _x( 'Upload Large Files', 'Modules: Uploader', 'geditorial-admin' ),
+			'icon'     => [ 'misc-16', 'upload' ],
+			'access'   => 'beta',
+			'frontend' => FALSE,
+			'keywords' => [
+				'upload',
+				'adminwidget',
+			],
+		];
+	}
+
+	protected function get_global_settings()
+	{
+		return [
+			'_dashboard' => [
+				'dashboard_widgets',
+				[
+					'field'       => 'form_before',
+					'type'        => 'textarea-quicktags',
+					'title'       => _x( 'Before Form', 'Setting Title', 'geditorial-uploader' ),
+					'description' => _x( 'Message to display before contents on admin dashbaord widget.', 'Setting Description', 'geditorial-uploader' ),
+				],
+			],
+			'_roles' => [
+				'uploads_roles' => [ NULL, $this->get_settings_default_roles() ],
+			],
+		];
+	}
+
+	public function init()
+	{
+		parent::init();
+
+		$this->action( 'post-plupload-upload-ui', 0, 12 );
+	}
+
+	public function dashboard_widgets()
+	{
+		if ( ! $this->role_can( 'uploads' ) )
+			return;
+
+		if ( $this->add_dashboard_widget( 'largefile', _x( 'Large File Uploader', 'Widget Title', 'geditorial-uploader' ), 'info' ) )
+			$this->enqueue_asset_js( [], $this->dotted( 'largefile' ) );
+	}
+
+	// @REF: https://github.com/deliciousbrains/wp-dbi-file-uploader
+	// @REF: https://deliciousbrains.com/?p=26646
+	public function render_widget_largefile( $object, $box )
+	{
+		if ( $this->check_hidden_metabox( $box ) )
+			return;
+
+		Core\HTML::desc( $this->get_setting( 'form_before', '' ), TRUE, '-intro' );
+
+		$html = '<form>'.Ajax::spinner();
+		$html.= '<div id="'.$this->classs( 'largefile', 'progress' ).'" class="-messages">';
+		$html.= _x( 'Please select a file and click &#8220;Upload&#8221; to continue.', 'Message', 'geditorial-uploader' );
+		$html.= '</div>';
+
+		$html.= '<div><label for="'.$this->classs( 'largefile', 'input' ).'" class="button button-small">';
+		$html.= _x( 'Select File', 'Button', 'geditorial-uploader' ).'</label>';
+
+		$html.= Core\HTML::tag( 'input', [
+			'id'    => $this->classs( 'largefile', 'input' ),
+			'type'  => 'file',
+			'style' => 'display:none',
+		] );
+
+		$html.= Core\HTML::tag( 'input', [
+			'disabled' => TRUE,
+			'type'     => 'submit',
+			'id'       => $this->classs( 'largefile', 'submit' ),
+			'value'    => _x( 'Upload', 'Button', 'geditorial-uploader' ),
+			'class'    => [ 'button', 'button-small', 'button-primary' ],
+			'data'     => [
+				'nonce'    => wp_create_nonce( $this->classs( 'largefile' ) ),
+				'locale'   => Core\L10n::locale(),
+				/* translators: `%s`: progress percent */
+				'progress' => _x( 'Uploading File - %s%', 'JS String', 'geditorial-uploader' ),
+				'complete' => _x( 'Upload Complete!', 'JS String', 'geditorial-uploader' ),
+			],
+		] );
+
+		$html.= '</div><code id="'.$this->classs( 'largefile', 'name' ).'" class="-filename" style="display:none"></code></form>';
+
+		echo $this->wrap( $html, '-admin-widget -widget-form' );
+	}
+
+	protected function get_widget_largefile_info()
+	{
+		return _x( 'You can access uploaded files via Media Library.', 'Widget Info', 'geditorial-uploader' );
+	}
+
+	public function do_ajax()
+	{
+		$post = self::unslash( $_REQUEST );
+		$what = empty( $post['what'] ) ? 'nothing': trim( $post['what'] );
+
+		switch ( $what ) {
+
+			case 'upload_check':
+
+				Ajax::checkReferer( $this->classs( 'largefile' ) );
+
+				$stored = $this->_handle_upload_check( $post['file'], $post['mime'] );
+
+				if ( TRUE === $stored )
+					Ajax::success();
+				else
+					Ajax::error( $stored );
+
+				break;
+
+			case 'upload_chaunk':
+
+				Ajax::checkReferer( $this->classs( 'largefile' ) );
+
+				$stored = $this->_handle_store_chaunk( $post['file'], $post['file_data'], (int) $post['chunk'] );
+
+				if ( TRUE === $stored )
+					Ajax::success();
+				else
+					Ajax::error( $stored );
+
+				break;
+
+			case 'upload_complete':
+
+				Ajax::checkReferer( $this->classs( 'largefile' ) );
+
+				$completed = $this->_handle_complete_upload( $post['file'] );
+
+				if ( $completed[0] )
+					Ajax::success( $completed[1] );
+				else
+					Ajax::errorMessage( $completed[1] );
+		}
+
+		Ajax::errorWhat();
+	}
+
+	private function _handle_upload_check( $filename, $mime )
+	{
+		$type = wp_check_filetype( $filename );
+
+		if ( empty( $type['type'] ) )
+			return _x( 'The mime-type is not allowed on this site!', 'Message', 'geditorial-uploader' );
+
+		$wpupload = WordPress\Media::upload();
+
+		if ( FALSE !== $wpupload['error'] )
+			return _x( 'Can not access upload folders!', 'Message', 'geditorial-uploader' );
+
+		if ( ! Core\File::writable( $wpupload['path'] ) )
+			return _x( 'The upload directory is not writable!', 'Message', 'geditorial-uploader' );
+
+		$path = Core\File::join( $wpupload['path'], sanitize_file_name( $filename ) );
+
+		if ( file_exists( $path ) )
+			return _x( 'The file is already exists in upload folder!', 'Message', 'geditorial-uploader' );
+
+		return TRUE;
+	}
+
+	// @REF: `media_handle_upload()`
+	private function _handle_complete_upload( $filename, $metadata = FALSE )
+	{
+		$wpupload = WordPress\Media::upload();
+
+		$file = sanitize_file_name( $filename );
+		$type = wp_check_filetype( $file );
+
+		$path = Core\File::join( $wpupload['path'], $file );
+		$url  = str_replace( $wpupload['basedir'], $wpupload['baseurl'], $file );
+
+		$ext   = pathinfo( $filename, PATHINFO_EXTENSION );
+		$name  = Core\File::basename( $filename, ".$ext" );
+		$title = sanitize_text_field( $name );
+
+		$id = wp_insert_attachment( [
+			'guid'           => $url,
+			'post_title'     => $title,
+			'post_mime_type' => $type['type'],
+		], $path, 0, TRUE );
+
+		if ( self::isError( $id ) )
+			return [ FALSE, $id->get_error_message() ];
+
+		if ( $metadata )
+			wp_update_attachment_metadata( $id, wp_generate_attachment_metadata( $id, $path ) );
+
+		return [
+			TRUE,
+			Core\HTML::link(
+				_x( 'Edit Uploaded Attachment', 'Message', 'geditorial-uploader' ),
+				Core\WordPress::getPostEditLink( $id ),
+				TRUE
+			)
+		];
+	}
+
+	private function _handle_store_chaunk( $file, $data, $chunk = 0 )
+	{
+		if ( FALSE === ( $decoded = $this->_do_decode_chunk( $data ) ) )
+			return _x( 'Something is wrong with data!', 'Message', 'geditorial-uploader' );
+
+		$wpupload = WordPress\Media::upload();
+
+		if ( FALSE !== $wpupload['error'] )
+			return _x( 'Can not access upload folders!', 'Message', 'geditorial-uploader' );
+
+		$path = Core\File::join( $wpupload['path'], sanitize_file_name( $file ) );
+
+		if ( 0 === $chunk && file_exists( $path ) )
+			return _x( 'The file is already exists in upload folder!', 'Message', 'geditorial-uploader' );
+
+		if ( ! file_put_contents( $path, $decoded, FILE_APPEND ) )
+			return _x( 'Can not put contents into file!', 'Message', 'geditorial-uploader' );
+
+		return TRUE;
+	}
+
+	private function _do_decode_chunk( $data )
+	{
+		$parts = explode( ';base64,', $data );
+
+		if ( ! is_array( $parts ) || ! isset( $parts[1] ) )
+			return FALSE;
+
+		return base64_decode( $parts[1] ) ?: FALSE;
+	}
+
+	public function post_plupload_upload_ui()
+	{
+		if ( ! $this->role_can( 'uploads' ) )
+			return;
+
+		Core\HTML::desc( sprintf(
+			/* translators: `%1$s`: link markup start, `%2$s`: link markup end */
+			_x( 'Alternatively, you can use %1$sLarge File Uploader%2$s widget on the dashoard.', 'Message', 'geditorial-uploader' ),
+			'<a href="'.Core\HTML::escapeURL( get_dashboard_url() ).'">', '</a>'
+		) );
+	}
+}
