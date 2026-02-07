40e5b8858af9663511721f22d02bb3b82ad8dfb9
Nasser Rafie <contact@geminorum.ir>
2026-02-07 19:56:57 +0330

    module/glossary : 🆕 module

---
+++ includes/Modules/Glossary/Glossary.php
@@ -0,0 +1,236 @@
+<?php namespace geminorum\gEditorial\Modules\Glossary;
+
+defined( 'ABSPATH' ) || die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+use geminorum\gEditorial;
+use geminorum\gEditorial\Core;
+use geminorum\gEditorial\Internals;
+use geminorum\gEditorial\Services;
+use geminorum\gEditorial\WordPress;
+
+class Glossary extends gEditorial\Module
+{
+	use Internals\BulkExports;
+	use Internals\ContentReplace;
+	use Internals\CoreAdmin;
+	use Internals\CoreCapabilities;
+	use Internals\CoreDashboard;
+	use Internals\CoreMenuPage;
+	use Internals\CoreRestrictPosts;
+	use Internals\DashboardSummary;
+	use Internals\MetaBoxSupported;
+	use Internals\TaxonomyOverview;
+	use Internals\TemplateTaxonomy;
+
+	protected $disable_no_posttypes = TRUE;
+
+	public static function module()
+	{
+		return [
+			'name'     => 'glossary',
+			'title'    => _x( 'Glossary', 'Modules: Glossary', 'geditorial-admin' ),
+			'desc'     => _x( 'Content List of Words', 'Modules: Glossary', 'geditorial-admin' ),
+			'icon'     => [ 'misc-16', 'journal-bookmark-fill' ],
+			'access'   => 'beta',
+			'keywords' => [
+				'has-shortcodes',
+				'taxmodule',
+			],
+		];
+	}
+
+	protected function get_global_settings()
+	{
+		$terms = WordPress\Taxonomy::listTerms( $this->constant( 'main_taxonomy' ) );
+		$empty = $this->get_taxonomy_label( 'main_taxonomy', 'no_items_available', NULL, 'no_terms' );
+
+		return [
+			'_general' => [
+				'autolink_terms' => [
+					_x( 'Tries to linkify the glossary elements in contents of supported post-types.', 'Settings', 'geditorial-glossary' ),
+					TRUE
+				],
+			],
+			'posttypes_option' => 'posttypes_option',
+			'_roles'           => $this->corecaps_taxonomy_get_roles_settings( 'main_taxonomy', TRUE, TRUE, $terms, $empty ),
+			'_dashboard'       => [
+				'dashboard_widgets',
+				'summary_parents',
+				'summary_excludes' => [ NULL, $terms, $empty ],
+				'summary_scope',
+				'summary_drafts',
+				'count_not',
+			],
+			'_editpost' => [
+				'metabox_advanced', // NOTE: if no `metabox_advanced` then no assignments!
+				'selectmultiple_term' => [ NULL, TRUE ],
+			],
+			'_editlist' => [
+				'admin_restrict',
+				'auto_term_parents',
+				'show_in_quickedit',
+			],
+			'_frontend' => [
+				'contents_viewable',
+				'show_in_navmenus',
+				'archive_override',
+				'custom_archives',
+			],
+			'_supports' => [
+				'shortcode_support',
+			],
+			'_constants' => [
+				'main_taxonomy_constant'  => [ NULL, 'glossary_elements' ],
+				'main_shortcode_constant' => [ NULL, 'glossary-elements' ],
+			],
+		];
+	}
+
+	protected function get_global_constants()
+	{
+		return [
+			'main_taxonomy'  => 'glossary_elements',
+			'main_shortcode' => 'glossary-elements',
+		];
+	}
+
+	protected function get_global_strings()
+	{
+		$strings = [
+			'noops' => [
+				'main_taxonomy' => _n_noop( 'Glossary Element', 'Glossary Elements', 'geditorial-glossary' ),
+			],
+			'labels' => [
+				'main_taxonomy' => [
+					'extended_label'       => _x( 'Glossary of Words', 'Label: `extended_label`', 'geditorial-glossary' ),
+					'menu_name'            => _x( 'Glossary', 'Label: `menu_name`', 'geditorial-glossary' ),
+					'show_option_all'      => _x( 'Elements', 'Label: `show_option_all`', 'geditorial-glossary' ),
+					'show_option_no_items' => _x( '(Undefined)', 'Label: `show_option_no_items`', 'geditorial-glossary' ),
+				],
+			],
+		];
+
+		if ( ! is_admin() )
+			return $strings;
+
+		// $strings[''] = [];
+
+		return $strings;
+	}
+
+	public function init()
+	{
+		parent::init();
+
+		$this->register_taxonomy( 'main_taxonomy', [
+			'hierarchical'       => TRUE,
+			'show_in_menu'       => FALSE,
+			'meta_box_cb'        => $this->get_setting( 'metabox_advanced' ) ? NULL : FALSE,
+			'show_in_quick_edit' => (bool) $this->get_setting( 'show_in_quickedit' ),
+			'show_in_nav_menus'  => (bool) $this->get_setting( 'show_in_navmenus' ),
+			'data_length'        => _x( '20', 'Main Taxonomy Argument: `data_length`', 'geditorial-glossary' ),
+		], NULL, [
+			'is_viewable'     => $this->get_setting( 'contents_viewable', TRUE ),
+			'auto_parents'    => $this->get_setting( 'auto_term_parents', TRUE ),
+			'single_selected' => ! $this->get_setting( 'selectmultiple_term', TRUE ),
+			'custom_captype'  => TRUE,
+		] );
+
+		$this->corecaps__handle_taxonomy_metacaps_roles( 'main_taxonomy' );
+		$this->coreadmin__ajax_taxonomy_multiple_supported_column( 'main_taxonomy' );
+		$this->templatetaxonomy__hook_custom_archives( 'main_taxonomy' );
+		$this->hook_dashboardsummary_paired_post_summaries( 'main_taxonomy' );
+		$this->bulkexports__hook_tabloid_term_assigned( 'main_taxonomy' );
+
+		$this->register_shortcode( 'main_shortcode', FALSE, 'glossary' );
+	}
+
+	public function current_screen( $screen )
+	{
+		if ( $this->constant( 'main_taxonomy' ) == $screen->taxonomy ) {
+
+			$this->_hook_parentfile_for_optionsgeneralphp();
+			$this->modulelinks__register_headerbuttons();
+			$this->bulkexports__hook_supportedbox_for_term( 'main_taxonomy', $screen );
+			$this->coreadmin__hook_taxonomy_multiple_supported_column( $screen );
+
+		} else if ( $this->posttype_supported( $screen->post_type ) ) {
+
+			if ( 'edit' == $screen->base ) {
+
+				if ( $this->corecaps_taxonomy_role_can( 'main_taxonomy', 'reports' ) )
+					$this->corerestrictposts__hook_screen_taxonomies( 'main_taxonomy' );
+
+			} else if ( 'post' === $screen->base ) {
+
+				// NOTE: if no `metabox_advanced` then no assignments!
+				// if ( ! $this->get_setting( 'metabox_advanced' ) )
+				// 	$this->hook_taxonomy_metabox_mainbox(
+				// 		'main_taxonomy',
+				// 		$screen->post_type,
+				// 		$this->get_setting( 'selectmultiple_term', TRUE )
+				// 			? '__checklist_restricted_terms_callback'
+				// 			: '__singleselect_restricted_terms_callback'
+				// 	);
+			}
+		}
+	}
+
+	public function admin_menu()
+	{
+		$this->_hook_menu_taxonomy( 'main_taxonomy', 'options-general.php' );
+	}
+
+	public function dashboard_widgets()
+	{
+		$this->add_dashboard_term_summary( 'main_taxonomy' );
+	}
+
+	public function cuc( $context = 'settings', $fallback = '' )
+	{
+		return $this->_override_module_cuc_by_taxonomy( 'main_taxonomy', $context, $fallback );
+	}
+
+	public function template_redirect()
+	{
+		if ( is_robots() || is_favicon() || is_feed() )
+			return;
+
+		if ( ! is_singular( $this->posttypes() ) )
+			return;
+
+		$this->contentreplace__autolink_terms( 'main_taxonomy' );
+	}
+
+	public function template_include( $template )
+	{
+		return $this->get_setting( 'contents_viewable', TRUE )
+			? $this->templatetaxonomy__include( $template, $this->constant( 'main_taxonomy' ) )
+			: $template;
+	}
+
+	public function main_shortcode( $atts = [], $content = NULL, $tag = '' )
+	{
+		return gEditorial\ShortCode::listPosts( 'assigned',
+			'post',
+			$this->constant( 'main_taxonomy' ),
+			array_merge( [
+				'post_id'   => NULL,
+				'posttypes' => $this->posttypes(),
+			], (array) $atts ),
+			$content,
+			$this->constant( 'main_shortcode' )
+		);
+	}
+
+	public function reports_settings( $sub )
+	{
+		$this->check_settings( $sub, 'reports', 'per_page' );
+	}
+
+	protected function render_reports_html( $uri, $sub )
+	{
+		if ( ! $this->taxonomy_overview_render_table( 'main_taxonomy', $uri, $sub ) )
+			return gEditorial\Info::renderNoReportsAvailable();
+	}
+}
