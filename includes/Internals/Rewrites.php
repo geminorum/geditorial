2955ec53e5399adf7143fcde0f4114d195b8b35e
Nasser Rafie <contact@geminorum.ir>
2026-02-07 19:44:01 +0330

    internal/rewrites: handle use verbose page rules

--- includes/Internals/Rewrites.php
+++ includes/Internals/Rewrites.php
@@ -10,21 +10,78 @@ use geminorum\gEditorial\WordPress;
 trait Rewrites
 {
 
+	protected function rewrites__get_queryvar( $constant_prefix = 'main' )
+	{
+		return $this->constant( $constant_prefix.'_queryvar' );
+	}
+
+	protected function rewrites__get_endpoint( $constant_prefix = 'main', $query = NULL )
+	{
+		return $this->constant( $constant_prefix.'_endpoint',
+			$query ?? $this->rewrites__get_queryvar( $constant_prefix )
+		);
+	}
+
 	// $constant_prefix.'_endpoint' => 'endpoint',
 	// $constant_prefix.'_queryvar' => 'endpoint',
 	// @SEE: on taxonomies: https://core.trac.wordpress.org/ticket/33728
 	protected function rewrites__add_endpoint( $constant_prefix = 'main' )
 	{
-		if ( ! $query = $this->constant( $constant_prefix.'_queryvar' ) )
+		if ( ! $query = $this->rewrites__get_queryvar( $constant_prefix ) )
 			return FALSE;
 
+		$endpoint = $this->rewrites__get_endpoint( $constant_prefix, $query );
+
 		add_rewrite_endpoint(
-			$this->constant( $constant_prefix.'_endpoint', $query ),
+			$endpoint,
 			EP_PERMALINK | EP_PAGES,
 			$query
 		);
 
+		$this->rewrites__endpoint_verbose_page( $constant_prefix, $query, $endpoint );
+
 		$this->filter_append( 'query_vars', $query );
+	}
+
+	// @source `View All Posts Pages` 0.9.4 by `Erick Hitter`
+	// @link https://wordpress.org/plugins/view-all-posts-pages/
+	// NOTE: Extra rules needed if verbose page rules are requested.
+	protected function rewrites__endpoint_verbose_page( $constant_prefix = 'main', $query = NULL, $endpoint = NULL )
+	{
+		global $wp_rewrite;
+
+		if ( ! $wp_rewrite->use_verbose_page_rules )
+			return;
+
+		$query    = $query    ?? $this->rewrites__get_queryvar( $constant_prefix );
+		$endpoint = $endpoint ?? $this->rewrites__get_endpoint( $constant_prefix, $query );
+
+		// Build regex.
+		$regex  = substr( str_replace( $wp_rewrite->rewritecode, $wp_rewrite->rewritereplace, $wp_rewrite->permalink_structure ), 1 );
+		$regex  = trailingslashit( $regex );
+		$regex .= $endpoint.'/?$';
+
+		// Build corresponding query string.
+		$string = substr( str_replace( $wp_rewrite->rewritecode, $wp_rewrite->queryreplace, $wp_rewrite->permalink_structure ), 1 );
+		$string = explode( '/', $string );
+		$string = array_filter( $string );
+
+		$i = 1;
+
+		foreach ( $string as $key => $qv ) {
+			$string[$key].= '$matches['.$i.']';
+			$i++;
+		}
+
+		$string[] = $query.'=1';
+
+		$string = implode( '&', $string );
+
+		add_rewrite_rule(
+			$regex,
+			$wp_rewrite->index.'?'.$string,
+			'top'
+		);
 	}
 
 	protected function rewrites__add_tag( $constant_prefix = 'main', $posttypes = NULL )
