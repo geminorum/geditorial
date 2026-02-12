2e27026d46ad8203e962399cd3a3cafde42650b2
Nasser Rafie <contact@geminorum.ir>
2026-02-12 20:46:39 +0330

    service/avatars: initial gravatar support

--- includes/Services/Avatars.php
+++ includes/Services/Avatars.php
@@ -42,4 +42,66 @@ class Avatars extends gEditorial\Service
 			$args
 		);
 	}
+
+	// @REF: https://docs.gravatar.com/rest/hash/
+	public function urlGravatar( $email, $size = NULL, $default = NULL )
+	{
+		if ( empty( $email ) )
+			return FALSE;
+
+		return vsprintf( '//www.gravatar.com/avatar/%s?s=%d&d=%s', [
+			hash( 'sha256', strtolower( trim( $email ) ) ),
+			$size ?? static::DEFAULT_SIZE,
+			urlencode( $default ?? DEFAULT_VALUE )
+		] );
+	}
+
+	/**
+	 * Get either a Gravatar URL or complete image tag for a specified email address.
+	 * @source https://gravatar.com/site/implement/images/php/
+	 *
+	 * @param string $email The email address
+	 * @param int $size Size in pixels, defaults to `64px` [ 1 - 2048 ]
+	 * @param string $default_image_type Default image-set to use [ 404 | mp | `identicon` | `monsterid` | `wavatar` ]
+	 * @param bool $force_default Force default image always. By default false.
+	 * @param string $rating Maximum rating (inclusive) [ g | pg | r | x ]
+	 * @param bool $return_image True to return a complete `IMG` tag False for just the URL.
+	 * @param array $html_tag_attributes Optional, additional key/value attributes to include in the `IMG` tag.
+	 * @return string containing either just a URL or a complete image tag
+	 */
+	public static function getGravatar(
+		$email,
+		$size                = 64,
+		$default_image_type  = 'mp',
+		$force_default       = FALSE,
+		$rating              = 'g',
+		$return_image        = FALSE,
+		$html_tag_attributes = []
+	) {
+
+		$params = [
+			's' => htmlentities( $size ),
+			'd' => htmlentities( $default_image_type ),
+			'r' => htmlentities( $rating ),
+		];
+
+		if ( $force_default )
+			$params['f'] = 'y';
+
+		$url = sprintf( '%s/%s?%s',
+			'//www.gravatar.com/avatar',
+			hash( 'sha256', strtolower( trim( $email ) ) ),
+			http_build_query( $params )
+		);
+
+		if ( ! $return_image )
+			return $url;
+
+		$attributes = '';
+
+		foreach ( $html_tag_attributes as $key => $value )
+			$attributes.= sprintf( '%s="%s" ', $key, htmlentities( $value, ENT_QUOTES, 'UTF-8' ) );
+
+		return sprintf( '<img src="%s" %s/>', $url, $attributes );
+	}
 }
