623a16d4a7029b1cc8ebc3605ef9dbaa870e4002
Nasser Rafie <contact@geminorum.ir>
2026-07-28 06:01:16 +0330

    🔧 apps updated

--- assets/apps/import-items/build/main.asset.php
+++ assets/apps/import-items/build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-i18n', 'wp-url', 'xlsx'), 'version' => '51da88ceda4c03bf4e00');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-i18n', 'wp-url', 'xlsx'), 'version' => '36d3e4aac7d76544f866');
